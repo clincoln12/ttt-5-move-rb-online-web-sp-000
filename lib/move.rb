818f3025fc279c604bd0c155340819d0b3cafb6b
Chris Lincoln <chris.lincoln12@gmail.com>
2019-05-01 23:41:00 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,8 +11,8 @@ def input_to_index(value)
   value.to_i - 1 
 end
 
-def move(array, index, value = "X")
-  array[index] = value
+def move(board, index, value = "X")
+  board[index] = value
   
-  array
+  board
 end
