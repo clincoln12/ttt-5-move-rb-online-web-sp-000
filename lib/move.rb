edd8d156df1c943139339e39ad0fc82f2471772f
Chris Lincoln <chris.lincoln12@gmail.com>
2019-05-01 21:43:11 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -15,7 +15,7 @@ def move(array, index, value = "X")
 end
 
 board = [" ", " ", " "]
-def update_array_at_with(array, index, value)
+def update_array_at_with(array[0], index, value)
   array[index] = value
 end
 
